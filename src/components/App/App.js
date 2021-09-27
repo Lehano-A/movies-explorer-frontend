a9ca4dd7e152b57998c2f09298267388ff8f8a24
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-09-27 20:28:13 +0300

    Блок Footer сверстан

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
         <Header />
         <Main />
+        <Footer />
 
       </div>
     </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section className="Footer">
+      <p className="footer__info-project">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__date-nav-box">
+        <p className="footer__date">&copy; {new Date().getFullYear()}</p>
+        <nav>
+          <ul className="footer__nav">
+            <li>Яндекс.Практикум</li>
+            <li>Github</li>
+            <li>Вконтакте</li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  )
+}
+
+export default Footer;
