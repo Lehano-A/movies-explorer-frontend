b590c954fac0d74b2248380f694b5b39fb1aaffa
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-10-08 00:18:28 +0300

    Корректировка параметров модального окна для отображения ошибок

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -4,7 +4,7 @@ function Error({ error, handleCloseError }) {
   console.log(error)
   return (
     <div onClick={handleCloseError} className={`Error ${error && 'error_active'}`}>
-      <p className="error__message">К сожалению, данный запрос не может быть обработан</p>
+        <p className="error__message">К сожалению, данный запрос не может быть обработан</p>
     </div>
   )
 
